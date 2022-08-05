0b0041a8367cce1ac7805079ddeb1982de1bd028
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2022-08-05 12:04:48 +0800

    Revert "SAIN-2 | create homepage and other configs"

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ["src"],
-  },
-  env: {
-    CLIENT_MEMBER_URL: process.env.CLIENT_MEMBER_URL,
-    API_GATEWAY_URL: process.env.API_GATEWAY_URL,
-  },
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
